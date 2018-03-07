aaa22e74643f2ef50391c75ba3ce9c6335989b3b
Joao Viana <jguilherme_viana@hotmail.com>
2018-03-07 08:02:13 +0100

    changes on Booking Controller and Model + Routes (booking part)

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,5 +1,38 @@
 class BookingsController < ApplicationController
+  before_action :set_booking, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @bookings = current_user.bookings
+  end
 
 
+
+  def edit
+
+  end
+
+  def update
+    set_booking
+      # We should change status here. Status should be set to 'pending' by default first
+    @booking.status = "Pending confirmation"
+
+  end
+
+  def destroy
+    set_booking
+    @booking.destroy
+    redirect_to root
+  end
+
+  private
+
+  def booking_params
+    params.require(:require).permit(:status, :time_arrive, :time_leave)
+
+  end
+
+  def set_booking
+    @booking = Booking.find(params[:id])
+  end
 
 end
